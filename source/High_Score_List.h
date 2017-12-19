2bb9d8adc70bfda67aa56759535898b1a2359b74
Lucas Tjärnström <luctj776@student.liu.se>
2017-12-19 15:10:42 +0100

    Commented High_Score_List.h

--- source/High_Score_List.h
+++ source/High_Score_List.h
@@ -1,5 +1,6 @@
 /*
  * High_Score_List.h
+ This class handles player entries and display of the high score list.
  */
 
 #ifndef HIGH_SCORE_LIST_H
@@ -17,13 +18,12 @@
 class High_Score_List : public Game, public Entry
 {
 public:
-    High_Score_List(); // constructor
+    High_Score_List();
 
     void run();
     void show_score();
     void add_entry(std::unique_ptr<Entry>);
     void save_to_file();
-    //void load_to_file(std::string);
 
 protected:
     std::vector<std::unique_ptr<Entry>> entries {};
@@ -34,19 +34,18 @@ protected:
 
     void render();
     void process_events();
-
-  sf::Font caviar {};
-  sf::Text highscore_text {};
-  sf::Text score_text1 {};
-  sf::Text score_text2 {};
-  sf::Text score_text3 {};
-  sf::Text score_text4 {};
-  sf::Text score_text5 {};
-  sf::Text score_text6 {};
-  sf::Text score_text7 {};
-  sf::Text score_text8 {};
-  sf::Text score_text9 {};
-  sf::Text score_text10 {};
+    sf::Font caviar {};
+    sf::Text highscore_text {};
+    sf::Text score_text1 {};
+    sf::Text score_text2 {};
+    sf::Text score_text3 {};
+    sf::Text score_text4 {};
+    sf::Text score_text5 {};
+    sf::Text score_text6 {};
+    sf::Text score_text7 {};
+    sf::Text score_text8 {};
+    sf::Text score_text9 {};
+    sf::Text score_text10 {};
 
 };
 
