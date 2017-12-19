5f026fc039cda2885980f8d785b8f05b63302d38
Lucas Tjärnström <luctj776@student.liu.se>
2017-12-19 18:06:23 +0100

    Commented Start_Menu

--- source/Start_Menu.h
+++ source/Start_Menu.h
@@ -1,5 +1,6 @@
 /*
  * Start_Menu.h
+ This class handles the start menu.
  */
 
 #ifndef START_MENU_H
@@ -11,22 +12,22 @@
 
 class Start_Menu
 {
-public:
-    Start_Menu();
-    std::pair <std::string,bool> run();
-
-private:
-    std::string process_events();
-    void handle_player_input(sf::Mouse::Button button);
-    void render();
-
-    sf::RenderWindow window {};
-    Button start_button {};
-    Button high_score_button {};
-    Button quit_button {};
-
-    sf::Texture background_texture {};
-    sf::Sprite background_sprite {};
+ public:
+  Start_Menu();
+  std::pair <std::string,bool> run();
+  
+ private:
+  std::string process_events();
+  void handle_player_input(sf::Mouse::Button button);
+  void render();
+  
+  sf::RenderWindow window {};
+  Button start_button {};
+  Button high_score_button {};
+  Button quit_button {};
+  
+  sf::Texture background_texture {};
+  sf::Sprite background_sprite {};
 };
 
 #endif
